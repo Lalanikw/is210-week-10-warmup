3b0ffa6374aa217e96cf8736cc953f239ccc6ca3
Lalani Werake <lalanikw@gmail.com>
2015-04-07 05:51:44 +0000

    task_02.py

---
+++ task_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Dictionaries warmup 10."""
+
+import data
+
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BAND_NAMES = list(data.BANDS.keys())
