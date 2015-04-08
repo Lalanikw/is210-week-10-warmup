a6e1e225c852893068f71612a881f3249e58d3fd
Lalani Werake <lalanikw@gmail.com>
2015-04-08 04:47:54 +0000

    task_01.py

--- task_01.py
+++ task_01.py
@@ -3,9 +3,9 @@
 """Dictionaries warmup 10."""
 
 
-GRADE_DATA = {'Luke Skywalker':
-              {'Math': 'B', 'etiquette': 'B+', 'grammer': 'B', 'gym': 'A'},
-              'Han Solo':
-              {'math': 'A', 'etiquette': 'C-', 'grammer': 'B'},
-              'C-3PO':
-              {'math': 'C', 'etiquette': 'A+', 'grammer': 'A', 'gym': 'F'}}
+GRADE_DATA = {
+    'Luke Skywalker': {
+        'Math': 'B', 'etiquette': 'B+', 'grammer': 'B', 'gym': 'A'},
+    'Han Solo': {'math': 'A', 'etiquette': 'C-', 'grammer': 'B'},
+    'C-3PO': {'math': 'C', 'etiquette': 'A+', 'grammer': 'A', 'gym': 'F'}
+}
